e95c0e89a2051c788fa21e38bb2291879be569bd
Shreeya Bhuwalka <shreeyabhuwalka@gmail.com>
2020-04-15 04:14:13 +0530

    Nearby places in process

--- app/src/main/java/com/example/nearbyplace/MapsActivity.java
+++ app/src/main/java/com/example/nearbyplace/MapsActivity.java
@@ -16,6 +16,7 @@ import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -49,6 +50,8 @@ public class MapsActivity extends FragmentActivity implements
     private Location lastLocation;
     private Marker currentUserLocationMarker;
     private static final int Request_User_Location_Code = 99;
+    private double latitude,longitude;
+    private int ProximityRadius = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +68,10 @@ public class MapsActivity extends FragmentActivity implements
     }
     public void onClick(View v)
     {
+        String hospital = "hospital",school = "School",restaurant = "restaurant";
+        Object transferData[] = new Object[2];
+        GetNearbyPlaces getNearbyPlaces = new GetNearbyPlaces();
+
         switch (v.getId())
         {
             case R.id.search_address:
@@ -105,8 +112,55 @@ public class MapsActivity extends FragmentActivity implements
                     Toast.makeText(this, "Please write any location name", Toast.LENGTH_SHORT).show();
                 }
                 break;
+            case R.id.hospitals_nearby:
+                mMap.clear();
+                String url = getUrl(latitude,longitude,hospital);
+                transferData[0] = mMap;
+                transferData[1] = url;
+                getNearbyPlaces.execute(transferData);
+                Toast.makeText(this, "Searching for nearby hospital...", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Showing for nearby hospital...", Toast.LENGTH_SHORT).show();
+
+                break;
+
+            case R.id.schools_nearby:
+                mMap.clear();
+                url = getUrl(latitude, longitude, school);
+                transferData[0] = mMap;
+                transferData[1] = url;
+                getNearbyPlaces.execute(transferData);
+                Toast.makeText(this, "Searching for nearby school...", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Showing for nearby school...", Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.restaurants_nearby:
+                mMap.clear();
+                url = getUrl(latitude, longitude, restaurant);
+                transferData[0] = mMap;
+                transferData[1] = url;
+                getNearbyPlaces.execute(transferData);
+                Toast.makeText(this, "Searching for nearby restaurant...", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Showing for nearby restaurant...", Toast.LENGTH_SHORT).show();
+
+                break;
+
+
         }
     }
+
+    private String getUrl(double latitude,double longitude,String nearbyPlace)
+    {
+
+        StringBuilder googleURL = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        googleURL.append("location="+latitude+","+longitude);
+        googleURL.append("&radius="+ProximityRadius);
+        googleURL.append("&type="+nearbyPlace);
+        googleURL.append("&sensor=true");
+        googleURL.append("&key="+"AIzaSyC4TYIyQP3EAqJx3nWx7gNcNSEqdGe3Mvg");
+        Log.d("MapsActivity","url : "+googleURL.toString());
+        return googleURL.toString();
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -131,6 +185,8 @@ public class MapsActivity extends FragmentActivity implements
     @Override
     public void onLocationChanged(Location location) {
         lastLocation = location;
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
         if(currentUserLocationMarker!=null)
         {
             currentUserLocationMarker.remove();
